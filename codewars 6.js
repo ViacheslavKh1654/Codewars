5c23d0be75f8b7b7fdecbb54cc73cc8ea7b30b1e
ViacheslavKh1654 <viacheslav.fictional.user@gmail.com>
2024-11-19 19:38:48 +0100

    Add files via upload

---
+++ codewars 6.js
@@ -0,0 +1,12 @@
+/* Given a non-empty array of integers, return the result of multiplying the values together in order */
+
+
+function grow(x){
+  let result
+  return(
+  result = x.reduce((acc, num) => acc * num, 1)
+  )
+  }
+ 
+
+
